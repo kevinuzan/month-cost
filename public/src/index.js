c38a45c6db0cb6e02287122b4d7762a4686206c3
kevinuzan <86110041+kevinuzan@users.noreply.github.com>
2025-06-25 10:21:10 -0300

    de 1 pra cima

--- public/src/index.js
+++ public/src/index.js
@@ -8,7 +8,7 @@ let souLider = false;
 function sortearNumeros() {
     const sorteados = new Set();
     while (sorteados.size < 5) {
-        const num = Math.floor(Math.random() * 15);
+        const num = Math.floor(Math.random() * 14) + 1;
         sorteados.add(num);
     }
 
